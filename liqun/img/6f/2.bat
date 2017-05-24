c4a86ef23f4361c2e7ab82c4bf4b18114ed894f5
xjf <969508264@qq.com>
2017-05-24 13:27:39 +0800

    5-24-1

---
+++ liqun/img/6f/2.bat
@@ -0,0 +1,28 @@
+@ECHO OFF
+REM ╠Да©ясЁы
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+REM File List
+SET LIST=list.txt
+
+REM Log File
+SET LOG=exec.log
+
+REM Get file list
+IF EXIST "%LIST%" (
+  del %LIST%  >> %LOG%
+)
+dir/b/o-d/a *.jpg >> %LIST%
+
+FOR /F "tokens=1,2 delims=."  %%a IN (%LIST%) DO (
+
+  SET /a x=!x!+1
+  SET nm=00000!x!
+
+  REM file rename
+  ren %%a.%%b img!nm:~-2!.jpg
+  ECHO ren %%a.%%b PIC2!nm:~-3!.jpg >> %LOG%
+
+)
+
+ECHO Success!!! >> %LOG%
